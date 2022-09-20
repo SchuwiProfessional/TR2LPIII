8161ee0420e6a48b0f21063e08561d8260ae29cd
alvaroorellana <NPcSchuwiPro~>
2022-09-20 09:25:11 -0500

    Trabajofinal2

---
+++ EXTRAS/ConsultaCS.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `clientes` (`nombre`, `apellido`, `foto`) VALUES ('NOMBRE', 'APELLIDO', 'FOTO');
+UPDATE `clientes` SET `nombre`='NOMBRE1', `apellido`='APELLIDO1', `foto`='FOTO1' WHERE  `id`=1;
+DELETE FROM `clientes` WHERE  `id`=1;
+SELECT * FROM `clientes` LIMIT 1000;
---
+++ EXTRAS/bk_crud_net_cs.sql
@@ -0,0 +1,36 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.6.0-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para curso_cs
+DROP DATABASE IF EXISTS `curso_cs`;
+CREATE DATABASE IF NOT EXISTS `curso_cs` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `curso_cs`;
+
+-- Volcando estructura para tabla curso_cs.clientes
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `foto` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
